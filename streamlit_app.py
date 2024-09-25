792be19e709f40bb56dd66779fe1c5682f3b6aa5
Saad Albadea <122687577+AlbadeaSaad@users.noreply.github.com>
2024-09-25 13:49:31 +0300

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,7 @@
 import streamlit as st
 import pandas as pd
 import math
-from pathlib import Path
-from torchvision import transforms
 from PIL import Image
-import torch.nn as nn
 import numpy as np
 from ultralytics import YOLO
 import cv2
